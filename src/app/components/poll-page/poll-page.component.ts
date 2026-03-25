03b9b036653bd5ba046d586070be3f5dacf0eb38
Adrien MARCEAU <adrien.marceau@capgemini.com>
2026-03-25 16:06:17 +0100

    Added Poll-page component

---
+++ src/app/components/poll-page/poll-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poll-page',
+  imports: [],
+  templateUrl: './poll-page.component.html',
+  styleUrl: './poll-page.component.scss'
+})
+export class PollPageComponent {
+
+}
