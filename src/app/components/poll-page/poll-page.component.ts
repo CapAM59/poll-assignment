a6922f1540162e3a30b866c1a2f19022ed255eaf
CapAM59 <adrien.marceau@capgemini.com>
2026-03-25 16:19:12 +0100

    Added 3 components : survey-admin, probe and results

--- src/app/components/poll-page/poll-page.component.ts
+++ src/app/components/poll-page/poll-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { SurveyAdminComponent } from '../survey-admin/survey-admin.component';
+import { SurveyProbeComponent } from '../survey-probe/survey-probe.component';
+import { SurveyResultsComponent } from "../survey-results/survey-results.component";
+
 @Component({
   selector: 'app-poll-page',
-  imports: [],
+  imports: [SurveyAdminComponent, SurveyProbeComponent, SurveyResultsComponent],
   templateUrl: './poll-page.component.html',
   styleUrl: './poll-page.component.scss'
 })
---
+++ src/app/components/survey-probe/survey-probe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-probe',
+  imports: [],
+  templateUrl: './survey-probe.component.html',
+  styleUrl: './survey-probe.component.scss'
+})
+export class SurveyProbeComponent {
+
+}
