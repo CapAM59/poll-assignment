da4c05b00bd2e57db96cb7c285143a7be2df098a
Adrien MARCEAU <adrien.marceau@capgemini.com>
2026-03-26 14:24:36 +0100

    added each models and moved mydb in db folder

---
+++ src/app/models/answer.model.ts
@@ -0,0 +1,7 @@
+export interface Answer {
+    id: number;
+    vote: boolean;
+    timestamp: LocalDate;
+    questionid: number;
+    userid: number;
+}
---
+++ src/app/models/poll.model.ts
@@ -0,0 +1,5 @@
+export interface Poll {
+    id: number;
+    title: string;
+    userid: number;
+}
