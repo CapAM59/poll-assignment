4574516242f83bf14a1f30a41254bd6c708dbabf
CapAM59 <adrien.marceau@capgemini.com>
2026-04-03 15:38:38 +0200

    Added 404 route (#19)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { PollPageComponent } from './components/poll-page/poll-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'poll', pathMatch: 'full' },
-    { path: 'poll', component: PollPageComponent }
+    { path: 'poll', component: PollPageComponent },
+    { path: '**', redirectTo: 'poll', pathMatch: 'full' }
 ];
